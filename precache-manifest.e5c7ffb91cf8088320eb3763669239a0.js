4cb9ea6decafcb6242a141bcfd656a1db4403851
Alexey Nagorny <anagorny@wavesplatform.com>
2019-09-05 16:13:59 +0300

    Updates

---
+++ precache-manifest.e5c7ffb91cf8088320eb3763669239a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070d41594c2a379a3b146c4eabf7bd5e",
+    "url": "/pornhub-christmas-club/index.html"
+  },
+  {
+    "revision": "2243bbcdc986fcc72c58",
+    "url": "/pornhub-christmas-club/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "bf969271719acee310b2",
+    "url": "/pornhub-christmas-club/static/css/main.7e8fc81e.chunk.css"
+  },
+  {
+    "revision": "2243bbcdc986fcc72c58",
+    "url": "/pornhub-christmas-club/static/js/2.69e8f9ab.chunk.js"
+  },
+  {
+    "revision": "bf969271719acee310b2",
+    "url": "/pornhub-christmas-club/static/js/main.71a2b988.chunk.js"
+  },
+  {
+    "revision": "aaa081fb4c7529895097",
+    "url": "/pornhub-christmas-club/static/js/runtime~main.9a8f05dd.js"
+  }
+]);
